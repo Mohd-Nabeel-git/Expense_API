a494ef07399b5c35522916853571e45bf8bf633d
Tech1Wizard <mohdnabeel5036@gmail.com>
2025-08-17 19:47:07 +0530

    adding swagger UI and updating README

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 import dotenv from 'dotenv'
 import connectDB from './db/connect.js'
 import authRoutes from './routes/authRoutes.js'
@@ -11,12 +13,34 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
+// Swagger setup
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.json({
+    message: "💰 Expense Tracker API is live 🚀",
+    docs: "/api-docs",
+    endpoints: {
+      auth: {
+        register: "POST /api/auth/register",
+        login: "POST /api/auth/login"
+      },
+      expenses: {
+        add: "POST /api/expenses",
+        getAll: "GET /api/expenses",
+        update: "PUT /api/expenses/:id",
+        delete: "DELETE /api/expenses/:id",
+        summary: "GET /api/expenses/summary"
+      }
+    }
+  });
+});
+
 
 const startServer = async () => {
   try {
